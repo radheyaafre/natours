7e4b2d7f232a39ee4880f69261e7806c45c36d62
radheya afre <radheya.afre@oracle.com>
2020-08-15 19:38:01 +0530

    fixed payment issue

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -25,7 +25,7 @@ exports.getCheckoutSession = catchAsync(async(req, res, next) =>{
         line_items: [{
             name: `${tour.name} Tour`,
             description: `${tour.summary}`,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+            images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
             amount: tour.price * 100, //convert into sents
             currency: 'usd',
             quantity: 1
@@ -57,7 +57,7 @@ exports.createBookingCheckout = catchAsync (async(req, res, next)=>{
 const createBookingCheckout =async session => {
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email })).id;
-    const price = session.line_items[0].amount/100;
+    const price = session.display_items[0].amount/100;
     await Booking.create({
         tour,
         user,
